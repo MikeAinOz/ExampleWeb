d33fbd4872f044351dca5427e312db8b3e711bce
MikeAinOz <mike@mikeallen.com.au>
2021-07-04 15:52:42 +1000

    not refreshing

--- api/processForm/index.ts
+++ api/processForm/index.ts
@@ -6,19 +6,19 @@ interface key {
     RowKey: string;
 }
 
-const httpTrigger: AzureFunction = function (context: Context, req: HttpRequest): Promise<Void> {
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     const tableService = createTableService();
     const thisKey : key = {PartitionKey: "Test",RowKey: Guid.create().toString()};
     const tableName = "Request";
-    tableService.insertEntity(tableName,{...thisKey,...req.query},{ echoContent: true }, function (error, result, response) {
+    tableService.insertEntity(tableName,{...thisKey,...req.body},{ echoContent: true }, function (error, result, response) {
         if (!error) {
             context.res.status(200).json(response);
         } else {
              context.res.status(500).json({ error: error });
         }
     });
-
+    
 };
 
 export default httpTrigger;
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # Vanilla Html App
 
 This repo is used as a starter for a _very basic_ HTML web application using no front-end frameworks.
+      // https://gomakethings.com/serializing-form-data-with-the-vanilla-js-formdata-object/
+        // https://pqina.nl/blog/async-form-posts-with-a-couple-lines-of-vanilla-javascript/
+
+Added a Function to process the form, run test with swa start . --api api
